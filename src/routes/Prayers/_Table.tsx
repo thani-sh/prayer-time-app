1155d81a7cc7399e921aa38c9c6d8c67124efffe
Muhammed Thanish <mnmtanish@gmail.com>
2024-01-08 23:09:47 +0100

    feat: implement selecting the date

--- src/routes/Prayers/_Table.tsx
+++ src/routes/Prayers/_Table.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 
 const styles = StyleSheet.create({
   container: {
@@ -36,22 +36,22 @@ const styles = StyleSheet.create({
   },
 });
 
-function formatTime( hour: number, minute: number ): string {
-  	const h = String(hour > 12 ? hour - 12 : hour);
-		const m = minute.toString().padStart(2, '0');
-		const a = hour > 12 ? 'pm' : 'am';
-		return `${h}:${m} ${a}`;
+function formatTime(hour: number, minute: number): string {
+  const h = String(hour > 12 ? hour - 12 : hour);
+  const m = minute.toString().padStart(2, '0');
+  const a = hour > 12 ? 'pm' : 'am';
+  return `${h}:${m} ${a}`;
 }
 
 export interface Props {
-  entries: { key: string, val: { hour: number, minute: number } }[];
+  entries: {key: string; val: {hour: number; minute: number}}[];
 }
 
 export default (props: Props) => {
   return (
     <View style={styles.container}>
-      {props.entries.map(({ key, val }, i) => (
-        <View key={key} style={{ ...styles.row, borderTopWidth: i == 0 ? 0 : 1}}>
+      {props.entries.map(({key, val}, i) => (
+        <View key={key} style={{...styles.row, borderTopWidth: i === 0 ? 0 : 1}}>
           <Text style={styles.key}>{key}</Text>
           <Text style={styles.val}>{formatTime(val.hour, val.minute)}</Text>
         </View>
--- src/routes/Prayers/index.tsx
+++ src/routes/Prayers/index.tsx
@@ -1,13 +1,23 @@
-import { forDate } from '@thani-sh/prayer-time-lk';
-import React, { useState } from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Table from './Table';
+import {forDate} from '@thani-sh/prayer-time-lk';
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+import Dates from './_Dates';
+import Table from './_Table';
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  screenContent: {
+    flex: 1,
+    marginBottom: -60,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  screenBottom: {
+    marginBottom: 40,
   },
   screenTitle: {
     fontSize: 40,
@@ -21,19 +31,24 @@ export default () => {
   const times = forDate(date);
 
   const entries = [
-    { key: 'Fajr', val: times.fajr },
-    { key: 'Sunrise', val: times.sunrise },
-    { key: 'Dhuhr', val: times.dhuhr },
-    { key: 'Asr', val: times.asr },
-    { key: 'Asr (حَنَفِية)', val: times.asr_hanafi },
-    { key: 'Maghrib', val: times.maghrib },
-    { key: 'Isha', val: times.isha },
+    {key: 'Fajr', val: times.fajr},
+    {key: 'Sunrise', val: times.sunrise},
+    {key: 'Dhuhr', val: times.dhuhr},
+    {key: 'Asr', val: times.asr},
+    {key: 'Asr (حَنَفِية)', val: times.asr_hanafi},
+    {key: 'Maghrib', val: times.maghrib},
+    {key: 'Isha', val: times.isha},
   ];
 
   return (
     <View style={styles.container}>
-      <Text style={styles.screenTitle}>Prayer Time</Text>
-      <Table entries={entries} />
+      <View style={styles.screenContent}>
+        <Text style={styles.screenTitle}>Prayer Time</Text>
+        <Table entries={entries} />
+      </View>
+      <View style={styles.screenBottom}>
+        <Dates value={date} onChange={setDate} />
+      </View>
     </View>
   );
 };
